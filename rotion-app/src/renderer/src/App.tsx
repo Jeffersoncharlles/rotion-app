3a73f9a68ef031938fb0f0de50674c0101cc2a24
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-03 10:31:02 -0300

    chore:layout default

--- rotion-app/src/renderer/src/App.tsx
+++ rotion-app/src/renderer/src/App.tsx
@@ -1,17 +1,8 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import { Routes } from './routes'
 import './styles/global.scss'
 
 export const App = () => {
   return (
-    <div className='h-screen w-screen text-rotion-100 flex'>
-      <Sidebar />
-      <div className='flex-1 flex flex-col max-h-screen'>
-        <Header />
-
         <Routes />
-      </div>
-    </div>
   )
 }
--- rotion-app/src/renderer/src/routes/index.tsx
+++ rotion-app/src/renderer/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Router, Route } from 'electron-router-dom'
 import { Blank } from '../pages/blank'
 import { Document } from '../pages/document'
+import { Default } from '../pages/layouts/default'
 
 
 
@@ -8,10 +9,10 @@ export function Routes() {
   return (
     <Router
       main={
-        <>
-          <Route path="/" element={<Blank />} />
-          <Route path="/document" element={<Document />} />
-        </>
+        <Route path='/' element={<Default />}>
+            <Route path="/" element={<Blank />} />
+            <Route path="/document" element={<Document />} />
+          </Route>
       }
     />
   )
