0f083833deae8e56e624d78c57d2dbff283a7f4b
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-05 10:16:02 -0300

    chore: create editor using tiptap editor

---
+++ rotion-app/src/renderer/src/components/Editor/index.tsx
@@ -0,0 +1,45 @@
+import  Document  from "@tiptap/extension-document";
+import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
+import Placeholder from "@tiptap/extension-placeholder";
+import Typography from "@tiptap/extension-typography";
+import { EditorContent ,useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+
+
+export const Editor = () => {
+  const editor = useEditor({
+    extensions: [
+      Document.extend({
+        content: 'heading block*',//primeiro elemento sempre um titulo
+      }),
+      StarterKit.configure({
+        document: false
+      }),
+      Highlight,
+      Image.configure({
+        inline: true,
+      }),
+      Typography,
+
+      Placeholder.configure({
+        placeholder: 'Untitled',
+        emptyEditorClass:'before:content-[attr(data-placeholder)] before:text-gray-500 before:h-0 before:float-left before:pointer-events-none',
+      }),
+    ],
+    // content: '<h1>Back-end</h1><p>Esse e o documento que explica sobre back-end.</p>',
+    autofocus: 'end',
+    editorProps: {
+      attributes: {
+        class:'focus:outline-none prose prose-invert prose-headings:mt-0',
+      }
+    }
+
+  })
+
+    return(
+      <EditorContent className="w-[65ch]" editor={editor} />
+
+    );
+}
--- rotion-app/src/renderer/src/pages/document.tsx
+++ rotion-app/src/renderer/src/pages/document.tsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
+import { Editor } from "../components/Editor";
+import   {ToC} from '../components/ToC'
 
 
 export const Document = () => {
 
   return (
-    <main className='flex-1 flex items-center justify-center text-rotion-400'>
-      Document
-      <Link to="/" >Acessar Blank</Link>
+    <main className='flex-1 flex py-12 px-10 gap-8'>
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs uppercase">Table Of Content</span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Autenticante</ToC.Link>
+            <ToC.Link>Banco de dados</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 leading-relaxed flex flex-col items-center">
+       <Editor />
+      </section>
     </main>
   );
 }
