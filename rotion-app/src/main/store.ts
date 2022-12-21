89d70ef7c3a04860eada2b0ab5fefefa6c5591d2
jeffersoncharlles <contato@jefferdeveloper.com>
2022-12-21 18:45:47 -0300

    chore:add realease workflow

--- rotion-app/src/main/store.ts
+++ rotion-app/src/main/store.ts
@@ -1,11 +1,15 @@
 import Store from 'electron-store'
 import { Document } from '../shared/types/ipc'
+import { is } from '@electron-toolkit/utils'
 
 interface StoreType{
   documents: Record<string,Document>
 }
 
+
+
 export const store = new Store<StoreType>({
+  name: is.dev?'dev':'production',
   defaults: {
     documents:{},
   }
